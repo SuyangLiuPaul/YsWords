61b214b37a391d0ab1561cffcb70a8b990391aee
Paul Liu <81271326+SuyangLiuPaul@users.noreply.github.com>
2026-08-26 09:25:19 +1000

    Record the v1.4.169 bump

--- lib/constants/app_version.dart
+++ lib/constants/app_version.dart
@@ -2215,7 +2215,7 @@ const String kAppVersion = _envAppVersion == '' ? '1.3.113' : _envAppVersion;
 /// for the build, which in practice means dev workflow only.
 const String kAppReleaseTime = String.fromEnvironment(
   'APP_RELEASE_TIME',
-  defaultValue: '2026-08-25T21:38:22Z',
+  defaultValue: '2026-08-25T23:05:04Z',
 );
 
 /// Returns a user-locale-formatted release time string. Parses
